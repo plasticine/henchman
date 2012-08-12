407e144e0ba0d1c327c1b208d01113041482d85c
Justin Morris <justin@pixelbloom.com>
2012-08-12 14:32:35 +1000

    Henchman should be borgy.

--- henchman/__init__.py
+++ henchman/__init__.py
@@ -4,7 +4,11 @@ from .minion.minion import Minion
 
 class Henchman(object):
     """docstring for Henchman"""
+
+    __shared_state = {}
+
     def __init__(self,):
+        self.__dict__ = self.__shared_state
         self.queue = Queue()
 
     def add_build(self, build_data):
