c48ffee65f51a1b47a738f56ef6ef4b7f2ec297b
Justin Morris <justin@pixelbloom.com>
2012-08-18 16:23:13 +1000

    Refactor code class and tests to use subprocess instead of envoy

--- henchman/minion/code.py
+++ henchman/minion/code.py
@@ -1,6 +1,6 @@
-from henchman.settings import Settings
+from henchman.settings import settings
+from subprocess import Popen, PIPE
 from os import path
-import envoy
 
 
 class GitCommandNotFound(Exception):
@@ -11,45 +11,99 @@ class GitCommandError(Exception):
     pass
 
 
-class Code(object):
+class Response(object):
+    """
     """
 
+    def __init__(self, proc, stdout=None, stderr=None):
+        self.proc = proc
+        self._stdout = stdout
+        self._stderr = stderr
+
+    @property
+    def returncode(self):
+        return self.proc.returncode.rstrip()
+
+    @property
+    def stdout(self):
+        if self._stdout is None:
+            return self.proc.stdout.rstrip()
+        return self._stdout.rstrip()
+
+    @property
+    def stderr(self):
+        if self._stderr is None:
+            return self.proc.stderr.rstrip()
+        return self._stderr.rstrip()
+
+
+class Code(object):
+    """
     """
 
     def __init__(self, build):
+        self._stdout = ''
+        self._stderr = ''
         self.build = build
 
-    def _check_response_status(self, response):
-        if response.status_code != 0:
-            raise GitCommandError(response.std_err)
+    def update(self):
+        """
+        Update the code to be built. Either clone anew or reset to ref
+        """
+        self.git_binary = self._which_git()
+        if self.git_binary:
+            if self._repo_exists_for_build():
+                self._reset()
+            else:
+                self._clone()
+                self._reset()
 
     def _which_git(self):
         """
         Make sure that git is available
         `which` returns a 0 status code if the command was found, 1 if not
         """
-        return not envoy.run("which git").status_code
+        cmd = "which git"
+        response = self._command(cmd)
+
+        if response.proc.returncode == 0:
+            return response.stdout
+        raise GitCommandNotFound(response.stderr)
 
     def _clone(self):
         """
         git clone repo path
         git reset --hard refspec
         """
-        return envoy.run("git clone %s %s" % (self.build.repo_url, self.build.cwd))
+        cmd = "%s clone %s %s" % (self.git_binary, self.build.repo_url,
+            self.build.cwd)
+        return self._check_command_response(self._command(cmd))
 
     def _reset(self):
         """
         git reset --hard refspec
         """
-        return envoy.run("cd %s && git reset --hard %s" % (self.build.cwd, self.build.refspec))
+        cmd = "cd %s && %s reset --hard %s" % (self.build.cwd, self.git_binary,
+            self.build.refspec)
+        return self._check_command_response(self._command(cmd))
 
-    def update(self):
-        if self._which_git():
-            if path.exists(self.build.cwd):
-                self._check_response_status(self._reset())
-            else:
-                self._check_response_status(self._clone())
-                self._check_response_status(self._reset())
-            return
-        else:
-            raise GitCommandNotFound
+    def _repo_exists_for_build(self):
+        """
+        Check if there is a cloned repo for the build. Pretty primitive. :P
+        """
+        if path.exists(path.join(self.build.cwd, '.git')):
+            return True
+        return False
+
+    def _command(self, command):
+        """
+        Execute given command and return the process object
+        """
+        proc = Popen(command, shell=True, stdout=PIPE, stdin=PIPE)
+        stdout, stderr = proc.communicate(None)
+        return Response(proc, stdout, stderr)
+
+    def _check_command_response(self, response):
+        if response.proc.returncode != 0:
+            raise GitCommandError(response.stderr)
+        return response
--- henchman/minion/tests/test_code.py
+++ henchman/minion/tests/test_code.py
@@ -1,9 +1,10 @@
-from os import path, makedirs, rmdir
+from os import path
 from tempfile import mkdtemp
 from nose.tools import assert_equal, raises
-from mock import Mock
+from nose.exc import SkipTest
+from mock import Mock, patch
 from henchman.settings import Settings
-from ..code import Code, GitCommandNotFound, GitCommandError
+from ..code import Code, Response, GitCommandNotFound, GitCommandError
 from ..build import Build
 
 
@@ -16,60 +17,71 @@ class TestCode(object):
         }
         self.build = Build(self.build_fixture)
 
-    def _delete_build_cwd(self, build):
-        if path.exists(build.cwd):
-            rmdir(build.cwd)
-
-    @raises(GitCommandNotFound)
     def test_which_git(self):
         """
-        Check that we react properly if the `git` command is not available
+        Ensure that we can locate the git binary. We should store the path to
+        the git binary.
         """
+
+        proc_mock = Mock()
+        proc_mock.returncode = 0
+        response = Response(proc_mock, '/usr/local/bin/git', '')
         code = Code(self.build)
-        code._which_git = Mock(return_value=False)
-        code.update()
+        code._command = Mock(return_value=response)
+
+        assert_equal(code._which_git(), '/usr/local/bin/git')
 
     @raises(GitCommandError)
-    def test_failed_git_command(self):
+    def test_failing_git_command(self):
         """
-        Check that we catch any git errors that arise from calling _clone or _reset
+        Make sure we properly wrap up any failing git commands in an exception
         """
         code = Code(self.build)
 
-        response_mock = Mock()
-        response_mock.status_code = 1
-        response_mock.std_err = "HERP DERP!"
-
-        code._clone = Mock(return_value=response_mock)
-        code._reset = Mock(return_value=response_mock)
+        proc_mock = Mock()
+        proc_mock.returncode = 1
+        response = Response(proc_mock, '', 'herp derp')
+        code._command = Mock(return_value=response)
+        code._which_git = Mock(return_value='lol')
         code.update()
+
+    @raises(GitCommandNotFound)
+    def test_no_git_binary(self):
+        """
+        Make sure that we raise GitCommandNotFound if the git binary is
+        not available.
+        """
+        code = Code(self.build)
+
+        proc_mock = Mock()
+        proc_mock.status_code = 1
+        response = Response(proc_mock, '', 'herp derp')
+        code._command = Mock(return_value=response)
+        code.update()  # raises GitCommandNotFound
 
     def test_update_reset(self):
         """
-        Test that if the code path already exits then update is run.
+        If the build cwd exists then we should run a reset on the repo
         """
         code = Code(self.build)
         code._clone = Mock()
         code._reset = Mock()
-        code._check_response_status = Mock()
+        code._repo_exists_for_build = Mock(return_value=True)
 
-        self._delete_build_cwd(self.build)
-        makedirs(self.build.cwd)
         code.update()
 
         code._reset.assert_called_once_with()
         assert not code._clone.called
 
-    def test_update_clone(self):
+    def test_update_clone_reset(self):
         """
-        If the code path does not exist then we need to do a full clone
+        if the build cwd doesn't exist then the we need to clone and then reset
         """
         code = Code(self.build)
-        code._clone = Mock()
+        code._clone = code._reset = Mock()
         code._reset = Mock()
         code._check_response_status = Mock()
-
-        self._delete_build_cwd(self.build)
+        code._repo_exists_for_build = Mock(return_value=False)
 
         code.update()
 
